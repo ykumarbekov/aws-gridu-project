6dbe5c7753c737f48515ad9bbafc7765cf25a327
ykumarbekov <yermek.kumarbekov@gmail.com>
2020-05-23 16:33:13 +0300

    commit 23.05.2020-4

--- aws/configurator.sh
+++ aws/configurator.sh
@@ -7,25 +7,10 @@ yum install git -y
 git clone https://github.com/ykumarbekov/aws-gridu-project.git /opt/aws-gridu-project
 mkdir /opt/logs
 chmod +x /opt/aws-gridu-project/code/runner.sh
+chmod +x /opt/aws-gridu-project/aws/rds_init.sh
 echo "*/30 * * * * /opt/aws-gridu-project/code/runner.sh" > /tmp/usrcrontab
+echo "*/5 * * * * /opt/aws-gridu-project/aws/rds_init.sh" >> /tmp/usrcrontab
 crontab /tmp/usrcrontab
-# #####################
-# #####################
-echo "*** RDS Initializing ***"
-BUCKET="ykumarbekov-534348"
-PROJECT_FOLDER="/opt/aws-gridu-project"
-echo "Copying rds.id to local folder..."
-#aws s3 mv s3://$BUCKET/config/rds.id /opt/
-aws s3api get-object --bucket ${BUCKET} --key config/rds.id /opt/rds.id
-echo "Finished"
-ENDPOINT=$(cat /opt/rds.id|awk -F "|" '{ print $1 }')
-DBUSER=$(cat /opt/rds.id|awk -F "|" '{ print $2 }')
-PGPASSWORD=$(cat /opt/rds.id|awk -F "|" '{ print $3 }')
-DB="db1"
-# #####################
-echo "Creating table..."
-psql -h $ENDPOINT -p 5432 -U $DBUSER -w -d $DB -f ${PROJECT_FOLDER}/aws/rds_catalog_table.sql
-echo "Finished"
-echo "Populating table..."
-export Q="copy catalog from stdin with delimiter as '|' NULL As '' CSV HEADER"
-cat ${PROJECT_FOLDER}/catalog/books.csv | psql -h $ENDPOINT -p 5432 -U $DBUSER -w -d $DB -c "${Q}"
+
+
+
--- aws/rds_init.sh
+++ aws/rds_init.sh
@@ -3,16 +3,18 @@
 BUCKET="ykumarbekov-534348"
 PROJECT_FOLDER="/opt/aws-gridu-project"
 
+test -e /opt/rds.id && exit 0
+
 aws s3 mv s3://$BUCKET/config/rds.id /opt/
+#aws s3api get-object --bucket ${BUCKET} --key config/rds.id /opt/rds.id
 
-export ENDPOINT=$(cat /opt/rds.id|awk -F "|" '{ print $1 }')
-export DBUSER=$(cat /opt/rds.id|awk -F "|" '{ print $2 }')
-export PGPASSWORD=$(cat /opt/rds.id|awk -F "|" '{ print $3 }')
-export DB="db1"
-
-# Creating table
-psql -h $ENDPOINT -p 5432 -U $DBUSER -w -d $DB -f ${PROJECT_FOLDER}/aws/rds_catalog.sql
-# Populate table
+ENDPOINT=$(cat /opt/rds.id|awk -F "|" '{ print $1 }')
+DBUSER=$(cat /opt/rds.id|awk -F "|" '{ print $2 }')
+PGPASSWORD=$(cat /opt/rds.id|awk -F "|" '{ print $3 }')
+DB="db1"
+# #####################
+# #####################
+psql -h $ENDPOINT -p 5432 -U $DBUSER -w -d $DB -f ${PROJECT_FOLDER}/aws/rds_catalog_table.sql
 export Q="copy catalog from stdin with delimiter as '|' NULL As '' CSV HEADER"
 cat ${PROJECT_FOLDER}/catalog/books.csv | psql -h $ENDPOINT -p 5432 -U $DBUSER -w -d $DB -c "${Q}"
 
